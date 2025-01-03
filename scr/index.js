2270447d022ec03c81736422f8fbb59c99b34066
GersonESS <gebhsantos@gmail.com>
2025-01-03 20:27:51 -0300

    a

--- scr/index.js
+++ scr/index.js
@@ -1,6 +1,26 @@
 const express = require('express');
 const app = express();
 const port = 3000;
+
 app.get('/', (req, res) => {
-  res.send('App rodando na porta 3000');dir
+    console.log('Entrou na rota get /');
+    res.send('App ' + ' - GET!'); 
+
+});
+
+app.post('/', (req, res) => {
+    console.log('Entrou na rota post /');
+    res.send('App - POST!');
+}
+);
+
+app.delete('/', (req, res) => {
+    console.log('Entrou na rota delete /');
+    res.send('App - DELETE!');
+});
+
+app.listen(port, () => {
+
+    console.log(`Server is running on port ${port}`);
+
 });
